6125318884d1f5ad0c0f467737e29b651462a639
Tyler Ketron <tketron@gmail.com>
2017-04-02 16:28:11 -0700

    completed fibonacci sequence method

--- oop_warmups.rb
+++ oop_warmups.rb
@@ -5,3 +5,13 @@ def my_reverse(input)
   end
   return reversed_string.join
 end
+
+def fibs(elements)
+  sequence = [0,1]
+  if elements > 2
+    (elements-2).times do
+      sequence.push(sequence[-2]+sequence[-1])
+    end
+  end
+  return sequence
+end
